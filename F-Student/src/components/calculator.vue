6bd1888b2c2bfbbec2cbe15c6de086e63d9defc9
Angelo Manlangit <manlangitangelomh@gmail.com>
2025-06-26 22:42:53 +0800

    added calculator component, removed other stuff from default vue

---
+++ F-Student/src/components/calculator.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="box">
+        <h1>Calculator</h1>
+        <input type="number">
+
+        <input type="number">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.box {
+    border-radius: 30px;
+    background: gray;
+    box-shadow: 5px;
+}
+</style>
