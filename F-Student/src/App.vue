afcb5e14e5ebbc5c0945d4840f4f78fd9eaf55cd
Angelo Manlangit <manlangitangelomh@gmail.com>
2025-06-28 21:52:58 +0800

    Calculator made

--- F-Student/src/App.vue
+++ F-Student/src/App.vue
@@ -1,6 +1,5 @@
 <template>
     <calculator>
-
     </calculator>
 </template>
 
--- F-Student/src/components/calculator.vue
+++ F-Student/src/components/calculator.vue
@@ -1,22 +1,133 @@
 <template>
-    <div class="box">
-        <h1>Calculator</h1>
-        <input type="number">
+    <div class="box flex-center">
+        <h1 
+            style="text-align: center; margin: 10% auto 10px">
+            🔥Calculator🔥</h1>
+        <form class="flex-center forms">
+            <input type="number" class="inputs" v-model="input_1">
 
-        <input type="number">
+            <select class="inputs" v-model="operator">
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">×</option>
+                <option value="/">÷</option>
+            </select>
+
+            <input type="number" class="inputs" v-model="input_2">
+            
+            <button  
+                class="inputs cool-button"
+                @click.prevent="displayResult()"
+                >
+                Calculate!
+            </button>
+        </form>
+        
+        <p
+            v-show="showResult"
+        ><b>{{ resultText }}</b></p>
     </div>
 </template>
 
 <script>
 export default {
+    props: ['is21'],
+    data() {
+        return {
+            resultText: '',
+            showResult: false,
+            is21: false
+        }
+    },
+    methods: {
+        displayResult() {
+            if (this.input_1 && this.operator && this.input_2) {
+                this.showResult = true;
+            }
 
+            if (this.input_1 == 9 && this.operator == "+" && this.input_2 == 10) {
+                this.resultText = 'Result: 21'
+                this.showResult = true;
+                this.is21 = true;
+            }
+            else {
+                this.resultText = 'Result: Hello World!'
+            }
+        }
+    }
 }
 </script>
 
 <style>
+.flex-center {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-content: center;
+}
+
 .box {
-    border-radius: 30px;
-    background: gray;
-    box-shadow: 5px;
+    background: white;
+    border: 1px solid rgb(225, 225, 225);
+    border-radius: 20px;
+    box-shadow: 5px 5px;
+
+    width: 16%;
+    max-width: 20%;
+
+    padding: 0px 10px;
+    
+    position: fixed;
+    left: 42%;
+    top: 2%;
+}
+
+.forms {
+    font-size: xx-large;
+    width: 90%;
+    max-width: 90%;
+}
+
+.inputs {
+    font-size: 20px;
+    height: 30px;
+    width: 100%;
+    max-width: 100%;
+    margin: 10px;
+}
+
+p {
+    font-size: 18px;
+    margin: 0px auto 20px;
+    text-align: center;
+}
+
+/* Button from CSS Scan */
+
+.cool-button {
+  background-color: #e8f9ee;
+  border-radius: 100px;
+  box-shadow: rgba(44, 187, 99, .2) 0 -25px 18px -14px inset,rgba(44, 187, 99, .15) 0 1px 2px,rgba(44, 187, 99, .15) 0 2px 4px,rgba(44, 187, 99, .15) 0 4px 8px,rgba(44, 187, 99, .15) 0 8px 16px,rgba(44, 187, 99, .15) 0 16px 32px;
+  color: rgb(20, 117, 20);
+  text-shadow: 0px 0px 0px rgb(23, 89, 23);
+  cursor: pointer;
+  display: inline-block;
+  padding: 0px;
+  text-align: center;
+  height: auto;
+  text-decoration: none;
+  transition: all 250ms;
+  border: 0px;
+  font-size: 25px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  margin-bottom: 15px;
+}
+
+.cool-button:hover {
+  box-shadow: rgba(44,187,99,.35) 0 -25px 18px -14px inset,rgba(44,187,99,.25) 0 1px 2px,rgba(44,187,99,.25) 0 2px 4px,rgba(44,187,99,.25) 0 4px 8px,rgba(44,187,99,.25) 0 8px 16px,rgba(44,187,99,.25) 0 16px 32px;
+  text-shadow: 3px 3px 5px rgb(84, 233, 84);
+  transform: scale(1.05) rotate(-1deg);
 }
 </style>
