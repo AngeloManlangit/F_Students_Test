3c1df819e88ea1db5e4a2590834f8927c62cf479
Angelo Manlangit <manlangitangelomh@gmail.com>
2025-08-10 19:19:58 +0800

    Updated config file

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/F_Student_Test/',
   plugins: [
     vue(),
     vueDevTools(),
